e015cb49997f8d8bab4942d14e3591d533dd2ecc
Maooookai <1220568032@qq.com>
2021-04-04 19:37:22 +0800

    Function Implements

--- src/main/java/cn/maoookai/ems/controller/user/ProfileController.java
+++ src/main/java/cn/maoookai/ems/controller/user/ProfileController.java
@@ -1,8 +1,17 @@
 package cn.maoookai.ems.controller.user;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
-
+@RequestMapping(value = "/user", name = "用户")
 public class ProfileController {
+
+    public ModelAndView profile(ModelAndView modelAndView, HttpSession httpSession){
+        return modelAndView;
+    }
+
 }
